bd6074fe470e8b5bb841771f59942c4b358cbf4f
YangYang <2bitlatch@gmail.com>
2023-06-04 16:50:03 +0800

    test

---
+++ chooeat/src/main/java/com/brain/chooeat/ChooeatApplication.java
@@ -0,0 +1,13 @@
+package com.brain.chooeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChooeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChooeatApplication.class, args);
+    }
+
+}
---
+++ chooeat/src/test/java/com/brain/chooeat/ChooeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brain.chooeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChooeatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
